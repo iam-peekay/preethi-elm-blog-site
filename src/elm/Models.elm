1013cd1eda20f3c0b112436131895cb25e5d5281
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-10-08 19:00:27 -0700

    Update model definition

--- src/elm/Models.elm
+++ src/elm/Models.elm
@@ -2,9 +2,27 @@ module Models exposing(..)
 
 import Routes.Routes exposing(Route)
 
+-- APP STATE
 type alias State =
-  { route : Route }
+  { route : Route
+  , fetching : Bool
+  , posts : Maybe (List Post)
+  , current : Maybe Post 
+  }
 
-newState : Route -> State
-newState route =
-  { route = route }
+-- SINGLE POST
+type alias Post =
+  { id : Int
+  , author : String
+  , title : String
+  , body : String
+  }
+
+-- INIT NEW STATE
+newState : Route -> Maybe Post -> State
+newState route post =
+  { route = route
+  , fetching = False
+  , posts = Nothing
+  , current = post
+  }

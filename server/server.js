580569be6a52f5d2352825f99866183a6d70b2ca
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-10-15 16:45:44 -0700

    Set up simple express server for handling API requests

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

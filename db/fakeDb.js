1361258c0eb9cf0a908562256bfdf0b8e83b8373
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-10-18 19:53:16 -0700

    Implement basic API endpoints for sending back posts and post by id

---
+++ db/fakeDb.js
@@ -0,0 +1,26 @@
+const filter = require('lodash/filter');
+
+const data = [
+  {
+    id: 0,
+    author: 'Preethi Kasireddy',
+    title: 'First Blog Post',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam maximus interdum ante sit amet tempor. Phasellus molestie facilisis nisl sit amet finibus. Sed congue purus in lectus tincidunt mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam in velit dui. Nullam in viverra ante, non imperdiet odio. Aenean sed malesuada tellus. Morbi turpis arcu, hendrerit eget convallis a, fringilla vitae lorem. Sed turpis enim, blandit quis neque non, ullamcorper condimentum mauris. Duis vel neque libero. Donec auctor tortor urna, non laoreet massa hendrerit sit amet. Vestibulum luctus cursus finibus. Morbi rutrum, leo sit amet maximus sollicitudin, turpis urna dapibus libero, vel finibus nunc mi vitae lectus. Mauris tempor congue ex a auctor.',
+  },
+  {
+    id: 1,
+    author: 'Preethi Kasireddy',
+    title: 'Second Blog Post',
+    body: 'Aliquam ultricies semper massa. Cras vulputate nisi sit amet justo vehicula auctor. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus justo sodales pellentesque scelerisque. Etiam eu lacus in sapien pharetra malesuada vel ut justo. Etiam tempus, felis ac laoreet tristique, purus lorem euismod mi, sit amet tincidunt leo orci sodales nunc. Aenean tortor libero, dapibus in maximus hendrerit, feugiat ut diam.',
+  },
+];
+
+module.exports = {
+  getPosts() {
+    return data;
+  },
+
+  getPost(id) {
+    return filter(data, ['id', id]);
+  },
+};
--- server/server.js
+++ server/server.js
@@ -3,6 +3,34 @@ const path = require('path');
 const app = express();
 const db = require('../db/fakeDb');
 
+/* Define API routes */
+
+// Get all posts
+app.get('/api/posts', (req, res) => {
+  const posts = db.getPosts();
+  if (posts) {
+    res.json(posts);
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
+// Get a specific post by id
+app.get('/api/posts/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = db.getPost(id);
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
+// For all other undefined routes, return 404
+app.get('/api/*', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
 app.get('/', (req, res) => {
   res.send('hello world');
 });

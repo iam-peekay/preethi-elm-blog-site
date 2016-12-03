52f4d52b4cbb28160132a9b472fe79219878b98d
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-12-03 15:51:10 -0800

    Update some minor styles

--- db/fakeDb.js
+++ db/fakeDb.js
@@ -4,14 +4,14 @@ const data = [
   {
     id: 0,
     author: 'Preethi Kasireddy',
-    title: 'First Blog Post',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam maximus interdum ante sit amet tempor. Phasellus molestie facilisis nisl sit amet finibus. Sed congue purus in lectus tincidunt mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam in velit dui. Nullam in viverra ante, non imperdiet odio. Aenean sed malesuada tellus. Morbi turpis arcu, hendrerit eget convallis a, fringilla vitae lorem. Sed turpis enim, blandit quis neque non, ullamcorper condimentum mauris. Duis vel neque libero. Donec auctor tortor urna, non laoreet massa hendrerit sit amet. Vestibulum luctus cursus finibus. Morbi rutrum, leo sit amet maximus sollicitudin, turpis urna dapibus libero, vel finibus nunc mi vitae lectus. Mauris tempor congue ex a auctor.',
+    title: 'When speaking to an audience',
+    body: 'Think about: 1) What from your past expeirence do you have in common? 2) Where are you headed in the future? What types of outcomes are mututally exclusive? and 3) Why are you uniquely qualified to be their guide? What similar journey have you gone on with a positive outcome?',
   },
   {
     id: 1,
     author: 'Preethi Kasireddy',
-    title: 'Second Blog Post',
-    body: 'Aliquam ultricies semper massa. Cras vulputate nisi sit amet justo vehicula auctor. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus justo sodales pellentesque scelerisque. Etiam eu lacus in sapien pharetra malesuada vel ut justo. Etiam tempus, felis ac laoreet tristique, purus lorem euismod mi, sit amet tincidunt leo orci sodales nunc. Aenean tortor libero, dapibus in maximus hendrerit, feugiat ut diam.',
+    title: 'Teaching programming',
+    body: 'Explain the concept without going into the nitty gritty of it. No need to go into the scary details.',
   },
 ];
 
--- src/elm/Components/About.elm
+++ src/elm/Components/About.elm
@@ -17,7 +17,7 @@ body =
   div [ class "about-body"] 
       [ p [] [ text ("Hi, I'm "), span [ class "highlight-word" ] [ text ("Preethi") ], text (" (Rhymes with leafy).") ]
       , p [] [ text ("I'm a ") , span [ class "highlight-word" ] [ text ("software engineer") ], text (" based in San Francisco.") ]
-      , p [] [ span [ class "highlight-word" ] [ text ("Building") ], text (" useful "), span [ class "highlight-word" ] [ text ("software") ], text (" software products is what gets me out of bed in the morning — aside from morning workouts, almond butter and my space heater, of course.") ]
+      , p [] [ span [ class "highlight-word" ] [ text ("Building") ], text (" useful and beautiful "), span [ class "highlight-word" ] [ text ("software") ], text (" products is what gets me out of bed in the morning — aside from morning workouts, almond butter and my space heater, of course.") ]
       , p [] [ span [ class "highlight-word" ] [ text ("Coding is my life") ], text (", although some of you know me from my former lives as a partner at Andreessen Horowitz, analyst at Goldman Sachs, and industrial engineering student at USC.") ]
       , p [] [ text ( "When I'm not immersed in "), span [ class "highlight-word" ] [ text ("JavaScript, React, Elm or Functional Programming") ], text(", you can find me giving back to the tech community through "), span [ class "highlight-word" ] [ text ("writing and teaching.") ] ]
       ]

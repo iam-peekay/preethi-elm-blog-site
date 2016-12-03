d19e0c816b3a77a454114fe49477c48a8543e1fa
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-12-03 15:27:42 -0800

    add about me section content and styles

--- src/elm/Components/About.elm
+++ src/elm/Components/About.elm
@@ -1,6 +1,6 @@
 module Components.About exposing (component)
 
-import Html exposing (Html, h1, div, text)
+import Html exposing (Html, h1, div, text, p, span)
 import Html.Attributes exposing (class)
 import Messages exposing (Msg)
 
@@ -9,5 +9,15 @@ component =
     div [ class "about-container" ]
         [ 
           h1 [ class "about-header" ] [ text ("About Me") ]
-        , div [ class "about-body" ] [ text "My name is Preethi" ]
+        , div [] [ (body) ]
         ]
+
+body : Html Msg
+body = 
+  div [ class "about-body"] 
+      [ p [] [ text ("Hi, I'm "), span [ class "highlight-word" ] [ text ("Preethi") ], text (" (Rhymes with leafy).") ]
+      , p [] [ text ("I'm a ") , span [ class "highlight-word" ] [ text ("software engineer") ], text (" based in San Francisco.") ]
+      , p [] [ span [ class "highlight-word" ] [ text ("Building") ], text (" useful "), span [ class "highlight-word" ] [ text ("software") ], text (" software products is what gets me out of bed in the morning — aside from morning workouts, almond butter and my space heater, of course.") ]
+      , p [] [ span [ class "highlight-word" ] [ text ("Coding is my life") ], text (", although some of you know me from my former lives as a partner at Andreessen Horowitz, analyst at Goldman Sachs, and industrial engineering student at USC.") ]
+      , p [] [ text ( "When I'm not immersed in "), span [ class "highlight-word" ] [ text ("JavaScript, React, Elm or Functional Programming") ], text(", you can find me giving back to the tech community through "), span [ class "highlight-word" ] [ text ("writing and teaching.") ] ]
+      ]
